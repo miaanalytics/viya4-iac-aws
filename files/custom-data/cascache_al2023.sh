9f88221300e014e1b343b44091b0690876f2f108
Eyal Gonen <eyal.gonen@miac.com>
2026-08-25 17:55:15 +0300

    fix: new improved cloud init script that handles dynamic devices

--- files/custom-data/cascache_al2023.sh
+++ files/custom-data/cascache_al2023.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
-mkfs -t xfs /dev/nvme1n1
-mkdir /cascache0
-mount /dev/nvme1n1 /cascache0
-chmod 777 /cascache0
+rootDevice=`findmnt / -o SOURCE -n`
+fixedRootdevice="${rootDevice%p[0-9]*}"
+
+counter=0
+
+for dev in /dev/nvme[0-9]n[0-9]; do
+    
+    # Check if the device exists to avoid errors
+    [ -e "$dev" ] || continue
+    
+    # Skip the fixed root device
+    if [ "$dev" = "$fixedRootdevice" ]; then
+        continue
+    fi
+
+    mkfs -t xfs $dev
+    mkdir /cascache$counter
+    mount $dev /cascache$counter
+    chmod 777 /cascache$counter
+
+    ((counter++))
+
+done
--- files/custom-data/saswork_al2023.sh
+++ files/custom-data/saswork_al2023.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
-mkfs -t xfs /dev/nvme1n1
-mkdir /saswork0
-mount /dev/nvme1n1 /saswork0
-chmod 777 /saswork0
+rootDevice=`findmnt / -o SOURCE -n`
+fixedRootdevice="${rootDevice%p[0-9]*}"
+
+counter=0
+
+for dev in /dev/nvme[0-9]n[0-9]; do
+    
+    # Check if the device exists to avoid errors
+    [ -e "$dev" ] || continue
+    
+    # Skip the fixed root device
+    if [ "$dev" = "$fixedRootdevice" ]; then
+        continue
+    fi
+
+    mkfs -t xfs $dev
+    mkdir /saswork$counter
+    mount $dev /saswork$counter
+    chmod 777 /saswork$counter
+
+    ((counter++))
+
+done
